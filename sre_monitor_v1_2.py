9226243813560b4a605ccb75b1a5c0a9883e2b02
LilianaLuvia <1643392077@qq.com>
2026-03-30 14:12:32 +0800

    refactor：为了适配sre_cron_manager.py的制作，重构了sre_monitor_v1_2.中的monitor_memmory方法，以防某种异常错误发生

--- sre_monitor_v1_2.py
+++ sre_monitor_v1_2.py
@@ -7,14 +7,14 @@ from modules import memory
 #配置环境变量
 base_dir=os.getcwd()
 log_dir=os.path.join(base_dir,"logs","health.log")
-MAX_SIZE=6*1024
+MAX_SIZE=10*1024
 FINAL=False
 
 #检查logs文件是否存在
 os.makedirs(os.path.join(base_dir,"logs"),exist_ok=True)
 
-#主程序运行
-if __name__=="__main__":
+#方法：完整冗余内存监控
+def monitor_memory(log_dir,MAX_SIZE):
     try:
         print("[@] 正在进行 [Log_Rorator] ")
         #调用轮转函数，并接收返回值，输出轮转情况
@@ -23,11 +23,18 @@ if __name__=="__main__":
         
         #备份程序完成，则进入下一步程序
         #调用内存检查函数，抓取系统当前的 available 内存数值
-        print("[@] 正在进行 [Memory_Check] ")
         if log_status is not False:
+            print("[@] 正在进行 [Memory_Check] ")
             success_info,mem_status,available_mem=memory.get_memory_info(log_dir)
+            return success_info,mem_status,available_mem
         else:
-            print("[@] [Memory_Check] 程序未正常进行 \n")
+            print("[@] [Log_Rorator] 程序发生未知异常 \n")
+            return False,None,None
         
     except Exception as e:
         print("** 发生未知异常！**")
+        return False,None,None
+
+#主程序运行
+if __name__=="__main__":
+    monitor_memory(log_dir,MAX_SIZE)    

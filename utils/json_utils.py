e9d15f80b51ad865ac478665a31753b633c98f4b
LilianaLuvia <1643392077@qq.com>
2026-04-07 00:32:41 +0800

    refactor： （1）将方法update_history()并入json_utils.py中 （2）简化了check_json_exist()方法的执行逻辑

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -1,23 +1,50 @@
 import os
+import json
+import traceback
+from datetime import datetime
 
 base_dir=os.getcwd()
-log_dir=os.path.join(base_dir,"logs")
 
 #方法：检查memory_history.json是否存在
 def check_json_exist():
-    result={
-        "Success":False,
-        "Json_Path":None
-    }
-    json_path=os.path.join(os.getcwd(),"logs","memory_history.json")
+    json_path=os.path.join(os.getcwd(),"logs","history.json")
     if not os.path.exists(json_path):  
-        return result
+        return False
     else :
-        result.update({
-            "Success":False,
-            "Json_Path":None
-        })
-        return result
+        return True
+    
+#方法：向history.json添加信息,可一次性自定义history.json文件位置
+def update_history(new_value,json_path=os.path.join(os.getcwd(),"logs","history.json")):
+    max_limit=10
+    try:
+        now=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        
+        #检查json文件存在与否,否则创建新的json
+        if os.path.exists(json_path):
+            with open(json_path,'r',encoding="UTF-8") as f:      
+                now_list=json.load(f)
+        else:
+            now_list=[]
+        #创建新字典
+        new_dir={"timestamp":now,
+            "value":f"{new_value}",
+            }
+        
+        #从列表头添加元素
+        now_list.insert(0,new_dir)
+        
+        #列表长度截断
+        now_list=now_list[:max_limit]
+        
+        #将列表写入Json
+        with open(json_path,'w') as f:
+            json.dump(now_list,f,indent=4,ensure_ascii=False)
+        
+        return True
+    except Exception:
+        traceback.print_exc()
+        return False
     
 if __name__=="__main__":
-    check_json_exist(log_dir)
+    check_json_exist()
+    update_history("测试信息")  

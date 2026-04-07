23a2a0d8606a5bb820aaf1ee8fc322bbd24b43d7
LilianaLuvia <1643392077@qq.com>
2026-04-07 16:13:52 +0800

    docs：更新了README

---
+++ utils/regex_utils.py
@@ -0,0 +1,15 @@
+import re
+
+test_text="Apr 7 00:02:01 debian sshd[1234]: Failed password for invalid user siliana from 192.168.1.100 port 54321 ssh2"
+
+def parse_ssh_log(line):
+    regex_pattern=r"for (?P<user>.*?) from (?P<ip>\d+\.\d+\.\d+\.\d+) port (?P<port>\d+)"
+    match=re.search(regex_pattern,line)
+    if match:     
+        return match.groupdict()
+    else:
+        return None
+
+if __name__=="__main__":
+    result=parse_ssh_log(test_text)
+    print(result)

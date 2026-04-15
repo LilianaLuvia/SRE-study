87314561a7c305070b0d3df6e506bdc84de63542
LilianaLuvia <1643392077@qq.com>
2026-04-15 17:16:58 +0800

    refactor： (1)对ip登录错误图标化添加了默认从大到小排序 (2)增加了parse_ssh_log的方法解释 (3)在logs文件夹下的__init__.py中添加了一间格式化时间戳的方法

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,8 @@ from utils import json_utils
 from utils import log_utils
 from utils import sys_utils
 from utils import regex_utils
+from datetime import datetime
+
+def get_time():
+    now=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return now

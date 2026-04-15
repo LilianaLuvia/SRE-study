efd236746d6afb6c3f757a18a092302a0dfdc366
LilianaLuvia <1643392077@qq.com>
2026-04-16 00:30:28 +0800

    fix：修复了start_watchdog()无法正常运行的问题

--- sre_monitor_loop.py
+++ sre_monitor_loop.py
@@ -1,4 +1,5 @@
 from sre_report_generator import generate_markdown_report
+from utils import ip_utils
 import time
 import os
 
@@ -10,12 +11,14 @@ def start_watchdog(interval:int,test_auth_log_path:str):
         count=1
         while 1:
             print(f"正在进行第 {count} 次巡检")
-            generate_markdown_report(test_auth_log_path)
+            parse_result=ip_utils.parse_ssh_log(test_auth_log_path)
+            ip_count=ip_utils.ip_counter(parse_result)
+            generate_markdown_report(ip_count)
             count+=1
             time.sleep(interval)
         
     except KeyboardInterrupt:
-        print("巡检结束")
+        print("\n巡检结束")
         
 if __name__=="__main__":
     start_watchdog(3,test_auth_log_path)

d08fb83877937a87c48b2ae08ed9d1a77da950fa
LilianaLuvia <1643392077@qq.com>
2026-04-15 23:13:24 +0800

    refactor：重构了大部分代码 （1）使得ip登录信息的提取解析不只是局限于单行或测试使用的列表，现在可以直接在日志文件中解析 （2）修改了check_log_exit()和write_to_log()的逻辑，现在需要传入日志文件名

--- sre_monitor_pro.py
+++ sre_monitor_pro.py
@@ -3,33 +3,27 @@ from utils import memory_utils
 from collections import Counter
 from utils import log_utils
 from utils import json_utils
+import os
 
-raw_auth_logs = [
-    "Apr 7 10:00:01 debian sshd[111]: Accepted password for siqi from 192.168.1.5 port 22 ssh2",
-    "Apr 7 10:01:05 debian sshd[123]: Failed password for root from 192.168.1.100 port 56789 ssh2",
-    "Apr 7 10:01:08 debian sshd[124]: Failed password for root from 192.168.1.100 port 56790 ssh2",
-    "Apr 7 10:01:12 debian sshd[125]: Failed password for invalid user admin from 192.168.1.101 port 43210 ssh2",
-    "Apr 7 10:01:15 debian sshd[126]: Failed password for root from 192.168.1.100 port 56791 ssh2",
-    "Apr 7 10:02:01 debian systemd[1]: Starting Daily Cleanup of Temporary Directories...",
-    "Apr 7 10:03:45 debian sshd[127]: Failed password for invalid user guest from 172.16.0.5 port 33221 ssh2",
-    "Apr 7 10:03:50 debian sshd[128]: Failed password for root from 192.168.1.100 port 56792 ssh2",
-    "Apr 7 10:04:10 debian kernel: [1234.56] Out of memory: Kill process 999 (malicious_bin)",
-    "Apr 7 10:05:00 debian sshd[129]: Failed password for root from 192.168.1.100 port 56793 ssh2"
-]
+test_auth_log_path=os.path.join(os.getcwd(),"logs","test_auth.log")
 
 #方法：执行安全审计
-def execute_sys_audit(raw_auth_logs):
+def execute_sys_audit(log_name):
+    #初始化工作目录
+    log_dir=os.path.join(os.getcwd(),'logs')
+    log_path=os.path.join(log_dir,log_name)
     
     #解析提取报错日志关键信息
+    all_logs=regex_utils.parse_ssh_log(log_path)
     failed_ips=[
-        parse.get("Ip")
-        for line in raw_auth_logs
-        if (parse:=regex_utils.parse_ssh_log(line)) and "Failed" in line
-    ]
+        item.get("Ip")
+        for item in all_logs
+        if "Failed" in item.get("Info","")
+    ] 
             
     #空列表判断
     if not failed_ips:
-        return
+        return False
             
     #抽取出现次数最多的IP
     ip_count=Counter(failed_ips)
@@ -43,17 +37,19 @@ def execute_sys_audit(raw_auth_logs):
     if float(mem_usage.get("Usage"))>90:
         msg=f"内存使用率已达{int(mem_usage.get("Usage"))}%"
         alert.append(msg)
-        log_utils.write_to_log(log_utils.format_alert_text("CRITICAL","High_Usage",msg))
+        log_utils.write_to_log(log_utils.format_alert_text("CRITICAL","High_Usage",msg),log_name)
         
     if count>3:
         msg=f"IP: {top_ip},失败次数: {count}"
         alert.append(msg)
-        log_utils.write_to_log(log_utils.format_alert_text("CRITICAL","Login Exception",msg))
+        log_utils.write_to_log(log_utils.format_alert_text("CRITICAL","Login Exception",msg),log_name)
         
     #批量存档history.json   
     for alert_msg in alert:
         json_utils.update_history(alert_msg)
+        
+    return True
     
 if __name__=="__main__":
-    execute_sys_audit(raw_auth_logs)
+    print(execute_sys_audit("test_auth.log"))
         
--- utils/regex_utils.py
+++ utils/regex_utils.py
@@ -1,16 +1,25 @@
 import re
+import os
 
-test_text="Apr 7 00:02:01 debian sshd[1234]: Failed password for invalid user siliana from 192.168.1.100 port 54321 ssh2"
+test_auth_log_path=os.path.join(os.getcwd(),"logs","test_auth.log")
 
 #方法：正则表达式截取ip登录信息
-def parse_ssh_log(line):
+def parse_ssh_log(auth_log_path):
     regex_pattern=r"]: (?P<Info>.*?) for (?P<User>.*?) from (?P<Ip>\d+\.\d+\.\d+\.\d+) port (?P<Port>\d+)"
-    match=re.search(regex_pattern,line)
-    if match:     
-        return match.groupdict()
-    else:
-        return None
+    parse_result=[]
+    
+    #提取日志文件内容
+    with open(test_auth_log_path,'r') as f:
+        for line in f:
+            clean_line=line
+            
+            #解析日志行
+            match=re.search(regex_pattern,clean_line)
+            if match:
+                parse_result.append(match.groupdict())
+
+    return parse_result
 
 if __name__=="__main__":
-    result=parse_ssh_log(test_text)
+    result=parse_ssh_log(test_auth_log_path)
     print(result)

7bb1722ac13c95a5187ffca2644b8dc03828e195
LilianaLuvia <1643392077@qq.com>
2026-04-17 17:29:31 +0800

    refactor： （1）为方法parse_ssh_log()增加了内部方法handle_stream()，用于统一传入的内容，现在可以传入日志文件地址、多行ip日志(list)、单行日志 （2）修改了ip_counter()的传入逻辑，现在传入的为可迭代对象Iterable

--- utils/ip_utils.py
+++ utils/ip_utils.py
@@ -1,28 +1,38 @@
 import re
 import os
 from collections import Counter
+from typing import Iterable
 
 test_auth_log_path=os.path.join(os.getcwd(),"logs","test_auth.log")
 
 #方法：正则表达式截取ip登录信息
-def parse_ssh_log(auth_log_path):
+def parse_ssh_log(source):
     regex_pattern=r"]: (?P<Info>.*?) for (?P<User>.*?) from (?P<Ip>\d+\.\d+\.\d+\.\d+) port (?P<Port>\d+)"
     parse_result=[]
     
-    #提取日志文件内容
-    with open(auth_log_path,'r') as f:
-        for line in f:
-            clean_line=line
-            
-            #解析日志行
-            match=re.search(regex_pattern,clean_line)
-            if match:
-                parse_result.append(match.groupdict())
-
-    return parse_result
+   #内部方法：统一不同输入源
+    def handle_stream(source):
+        if isinstance(source,str):
+            if os.path.exists(source):
+                with open(source,'r') as f:
+                    for line in f:
+                        yield line.strip()
+            else:
+                yield source.strip()
+        elif isinstance(source,list):
+            for line in source:
+                yield line
+        else:
+            raise ValueError("[ parse_ssh_log ] : 传入数据类型错误!")
+        
+    #统一进行解析
+    for i in handle_stream(source):
+        match=re.search(regex_pattern,i)
+        if match:
+            yield match.groupdict()
 
 #方法：统计各个ip的出现次数
-def ip_counter(parse_result:list):
+def ip_counter(parse_result:Iterable):
     return Counter(item["Ip"] for item in parse_result)
 
 #方法：生成ip登录错误次数进行图标化
@@ -42,4 +52,6 @@ def generate_ascii_bar(stats_dict:dict,reverse: bool=True):
         return None
     
 if __name__=="__main__":
-    print(generate_ascii_bar(test_auth_log_path))
+    ip_count=ip_counter(parse_ssh_log(test_auth_log_path))
+    print(ip_count)
+    print(generate_ascii_bar(ip_count))

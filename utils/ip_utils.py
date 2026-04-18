90ea5c77103a64fd7e23124b738cba20c24d3ccc
LilianaLuvia <1643392077@qq.com>
2026-04-19 01:44:11 +0800

    refactor：现在返回的字典为从大到小排序，并且添加了count，可指定返回前n项

--- utils/ip_utils.py
+++ utils/ip_utils.py
@@ -31,9 +31,10 @@ def parse_ssh_log(source):
         if match:
             yield match.groupdict()
 
-#方法：统计各个ip的出现次数
-def ip_counter(parse_result:Iterable):
-    return Counter(item["Ip"] for item in parse_result)
+#方法：统计各个ip的出现次数，返回从大到小排序,传入count以指定传回前n项
+def ip_counter(parse_result:Iterable,count:int=0):
+    counter_ip=Counter(item["Ip"] for item in parse_result)
+    return dict(counter_ip.most_common(count if count>0 else None))
 
 #方法：生成ip登录错误次数进行图标化
 def generate_ascii_bar(stats_dict:dict,reverse: bool=True):

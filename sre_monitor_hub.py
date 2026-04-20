54bc885ef796244512c4c2956b866e525048b4ac
LilianaLuvia <1643392077@qq.com>
2026-04-20 18:03:50 +0800

    feat：集合了大部分的数据，做成了数据肚饿聚合中心

---
+++ sre_monitor_hub.py
@@ -0,0 +1,20 @@
+import utils
+
+#方法：SRE 数据聚合中心
+def get_system_snapshot():
+    snapshot={
+        "Hardware":{
+            "Memory":utils.memory_u.get_memory_info(),
+            "Disk":utils.disk_u.get_disk_usage_report()
+        },
+        "Security":{
+            "Active_Ssh":utils.network_u.get_active_ssh_session()
+        },
+        "Process":{
+            "Top_Process":utils.process_u.get_cpu_processes()
+        }
+    }
+    return snapshot
+
+if __name__=="__main__":
+    utils.json_u.update_history(get_system_snapshot())

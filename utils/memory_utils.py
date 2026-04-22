7577fa1c72b4582a32d72a4bab0efb707922ee7b
LilianaLuvia <1643392077@qq.com>
2026-04-22 17:32:29 +0800

    refator: 完全重构了get_memory_info()的代码，使之适配在Docker中运行

--- utils/memory_utils.py
+++ utils/memory_utils.py
@@ -1,5 +1,5 @@
 import os
-import subprocess
+import psutil
 from datetime import datetime
 
 #方法: 数据清洗，清除"Gi"、"Mi"
@@ -14,52 +14,18 @@ def parse_to_mb(available_mem):
         return 0.0
     
 #方法: 查看完整内存状态及计算压力值Usage%
-def get_memory_info():
-    MAX_SIZE=20*1024
+def get_memory_info():  
+    #抓取系统当前的 available 内存数值
+    res=psutil.virtual_memory()
+    total_mem=res.total
+    used_mem=res.used
+    usage_mem=res.percent
     
-    #定义返回值字典
-    result={
-        "success":False,
-        "status":None,
-        "available_mem":None,
-        "usage":None
-        }
+    return {
+        "total_mem":f"{total_mem/(1024**2):.2f}",
+        "used_mem":f"{used_mem/(1024**2):.2f}",
+        "usage_mem":f"{usage_mem}"
+    }
         
-    try:
         
-        #抓取系统当前的 available 内存数值
-        res=subprocess.run(['free','-h'],capture_output=True,text=True,check=True)
-
-        #裁切"free -h"运行的结果
-        lines=res.stdout.splitlines()
-        parts=lines[1].split()
-
-        #提取总内存及冗余内存,数据清洗，去除单位
-        total_mem=parse_to_mb(parts[1])
-        available_mem=parse_to_mb(parts[6])
-
-        #逻辑判断内存
-        if available_mem<100:
-            is_low=True
-        else:
-            is_low=False
-        
-        status="内存告急" if is_low else "内存充足"
-        
-        #计算压力值Usage%
-        usage_percent=(total_mem-available_mem)/total_mem*100
-        
-        #将数据计入字典，程序运行返回数据以字典形式作返回值,以便后续分析使用
-        result.update({
-                "success":True,
-                "status":status,
-                "available_mem":available_mem,
-                "usage":f"{usage_percent:.2f}"
-            })
-            
-        return result
-        
-    except subprocess.CalledProcessError as e:
-        print(f"命令运行出错: {e}")
-        return result
 

31b3f9bf28d7d7d316812e2f907dda0bde35aa64
LilianaLuvia <1643392077@qq.com>
2026-04-23 22:39:55 +0800

    fix: 修复了每次方法运行时都会print磁盘数据的问题

--- utils/disk.py
+++ utils/disk.py
@@ -1,16 +1,15 @@
-import shutil
+import psutil
 
 #方法: 获取磁盘或者某个文件夹下的内存信息
 def get_disk_usage_report(path="/"):
     gb_unit=1024**3
-    res=shutil.disk_usage(path)
+    res=psutil.disk_usage(path)
     usage=res.used/res.total
     return {
         "total":f"{res.total/gb_unit:.2f}",
         "used":f"{res.used/gb_unit:.2f}",
         "free":f"{res.free/gb_unit:.2f}",
-        "usage":f"{usage*100:.2f}",
-        "status":"HEALTHY" if usage<0.85 else "DANGER"
+        "usage":f"{usage*100:.2f}"
     }
 
 if __name__=="__main__":

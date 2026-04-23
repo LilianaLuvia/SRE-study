5212bb0b8e8e72976e5a7df039c28c3ce3edd87e
LilianaLuvia <1643392077@qq.com>
2026-04-23 22:07:05 +0800

    refactor: 为ip_couinter的传入值count添加了默认值5,也就是现在默认会前5个

--- utils/ip.py
+++ utils/ip.py
@@ -7,7 +7,7 @@ import psutil
 test_auth_log_path=os.path.join(os.getcwd(),"logs","test_auth.log")
 auth_log_path=os.path.join("/var","log","auth.log")
 
-#方法: 正则表达式截取高频登录错误ip信息
+#方法: 正则表达式截取登录错误ip信息
 def parse_ssh_log(source):
     regex_pattern=r"]: FAILED SU \(to (?P<target_user>.*?)\) (?P<who>.*?) on"
     parse_result=[]
@@ -34,7 +34,7 @@ def parse_ssh_log(source):
             yield match.groupdict()
 
 #方法: 统计各个尝试登录用户的出现次数,返回从大到小排序,传入count以指定传回前n项
-def ip_counter(parse_result:Iterable,count:int=0):
+def ip_counter(parse_result:Iterable,count:int=5):
     counter_ip=Counter(item["who"] for item in parse_result)
     return dict(counter_ip.most_common(count if count>0 else None))
 
